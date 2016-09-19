7b3593486cf0228b12324dbb0daf5d395a65cfa0
Derek Fong <i.am@derekfong.me>
2016-09-20 00:27:51 +0800

    Test SCSS imports

--- client/imports/demo/demo.component.ts
+++ client/imports/demo/demo.component.ts
@@ -4,10 +4,13 @@ import { MeteorComponent } from 'angular2-meteor';
 import { DemoDataService } from './demo-data.service';
 
 import template from './demo.component.html';
+import style from './demo.component.scss';  // FIXME: won't work under `client/imports`
+// import style from '../../../imports/client/test.component.scss'; // REVIEW: Works under `imports/client`
 
 @Component({
   selector: 'demo',
   template,
+  styles: [style],
   providers: [DemoDataService]
 })
 export class DemoComponent extends MeteorComponent {
